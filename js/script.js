cacbb622adbfcf474e05fb1afee6b5a3b5a3c76f
MedvedievM <medvediev.m91@gmail.com>
2019-09-24 22:24:58 +0300

    add form data

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ let tab = document.querySelectorAll('.info-header-tab'),
 
     //======TIMER======
 
-    let deadline = '2019-09-23';
+    let deadline = '2019-12-23';
 
     function getTimeRemaining(endtime) {
         let t = Date.parse(endtime) - Date.parse(new Date()),           //parse - переводим в милисекунды
@@ -75,6 +75,72 @@ let tab = document.querySelectorAll('.info-header-tab'),
         }    
     };
 
-
     setClock('timer', deadline);
+
+    //Modal
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden'; 
+    });
+    
+    close.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.remove('more-splash');
+        document.body.style.overflow = ''; 
+    });
+
+    //Form
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    //отправка данных с формы на сервер  
+    form.addEventListener('submit', function() {
+        event.preventDefault();                 //Отменяем стандартное поведение браузера. во избежания перезагрузки страницы при оправки формы 
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader ('Content-type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        //конвертируем данные от пользователя в формат json
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        //наблюдаем за изменениями состояния запроса
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        //очищаем поле input после введения данных
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });    
 });
